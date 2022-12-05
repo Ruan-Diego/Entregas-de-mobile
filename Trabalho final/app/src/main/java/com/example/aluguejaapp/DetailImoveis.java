4eb7cc562094cc4de64a66283fde833f19cfcb52
Ruan Diego <Ruandgn123@gmail.com>
2022-12-05 06:03:27 -0300

    Add deleteimoveis

---
+++ Trabalho final/app/src/main/java/com/example/aluguejaapp/DetailImoveis.java
@@ -0,0 +1,112 @@
+package com.example.aluguejaapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.app.Activity;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class DetailImoveis extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+    SearchView searchView;
+
+    TextView ruaD, numD, bairroD, cidadeD, ufD, mensalD, quartosD, banheD, contatoD;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_imoveis);
+
+        ruaD = findViewById(R.id.txtRuaD);
+        numD = findViewById(R.id.txtNumD);
+        bairroD = findViewById(R.id.txtBairroD);
+        cidadeD = findViewById(R.id.txtCidadeD);
+        ufD = findViewById(R.id.txtUFD);
+        mensalD = findViewById(R.id.txtMensalD);
+        quartosD = findViewById(R.id.txtQuartosD);
+        banheD = findViewById(R.id.txtBanheD);
+        contatoD = findViewById(R.id.txtContD);
+
+        if (getIntent().getExtras() != null) {
+            String rua = (String) getIntent().getExtras().get("rua");
+            String num = (String) getIntent().getExtras().get("numero");
+            String bairro = (String) getIntent().getExtras().get("bairro");
+            String cidade = (String) getIntent().getExtras().get("cidade");
+            String uf = (String) getIntent().getExtras().get("uf");
+            String mensal = (String) getIntent().getExtras().get("mensalidade");
+            String quartos = (String) getIntent().getExtras().get("quartos");
+            String banhe = (String) getIntent().getExtras().get("banheiros");
+            String contato = (String) getIntent().getExtras().get("contato");
+
+            ruaD.setText(rua);
+            numD.setText(num);
+            bairroD.setText(bairro);
+            cidadeD.setText(cidade);
+            ufD.setText(uf);
+            mensalD.setText(mensal);
+            quartosD.setText(quartos);
+            banheD.setText(banhe);
+            contatoD.setText(contato);
+        }
+
+        searchView = findViewById(R.id.idSearchView);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                String location = searchView.getQuery().toString();
+                List<Address> addressList = null;
+
+
+                Geocoder geocoder = new Geocoder(DetailImoveis.this);
+                try {
+                    addressList = geocoder.getFromLocationName(location, 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                Address address = addressList.get(0);
+
+                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                map.addMarker(new MarkerOptions().position(latLng).title(location));
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17f));
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng Ceara = new LatLng(-5.489161114975658, -39.31813813058585);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Ceara, 7f));
+    }
+}
---
+++ Trabalho final/app/src/main/java/com/example/aluguejaapp/TelaInicial.java
@@ -0,0 +1,193 @@
+package com.example.aluguejaapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.aluguejaapp.transactions.Constants;
+import com.example.aluguejaapp.model.Imoveis;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class TelaInicial<childEventListener> extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    DatabaseReference databaseReference;
+    ListView listView;
+    ArrayList<Imoveis> arrayList = new ArrayList<>();
+    ArrayAdapter<Imoveis> arrayAdapter;
+    int select;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_tela_inicial);
+        databaseReference = FirebaseDatabase.getInstance().getReference("Imoveis");
+        listView = (ListView) findViewById(R.id.listviewtxt);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+        select = -1;
+
+        Button alugar = findViewById(R.id.btnAlugar);
+
+        listView.setSelector(android.R.color.holo_blue_dark);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                Toast.makeText(TelaInicial.this, "" + arrayList.get(position).toString(), Toast.LENGTH_SHORT).show();
+                select = position;
+            }
+        });
+
+        alugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Imoveis imovel = arrayList.get(select);
+                String numContato = imovel.getContato();
+
+                String message = "Olá, me interessei pelo seu imóvel que fica no endereço " + imovel.getRua() + ", " + imovel.getNumero() + ", que fica no " + imovel.getBairro() + ".";
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://api.whatsapp.com/send/?phone="+"+55"+numContato + "&text=" + message));
+                startActivity(intent);
+            }
+        });
+
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {
+                Imoveis imovel = dataSnapshot.getValue(Imoveis.class);
+                arrayList.add(imovel);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_tela_inicial, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void onClickDetails (View view) {
+        Intent intent = new Intent(this, DetailImoveis.class);
+
+        Imoveis imovel = arrayList.get(select);
+        intent.putExtra("id", imovel.getId());
+        intent.putExtra("bairro", imovel.getBairro());
+        intent.putExtra("banheiros", imovel.getBanheiros());
+        intent.putExtra("cidade", imovel.getCidade());
+        intent.putExtra("contato", imovel.getContato());
+        intent.putExtra("mensalidade", imovel.getMensalidade());
+        intent.putExtra("numero", imovel.getNumero());
+        intent.putExtra("quartos", imovel.getQuartos());
+        intent.putExtra("rua", imovel.getRua());
+        intent.putExtra("uf", imovel.getUf());
+
+        startActivity(intent);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(TelaInicial.this, "" + item.getItemId(), Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+            case R.id.profile:
+                perfilUsuario(mAuth.getCurrentUser());
+                break;
+            case R.id.properties:
+                addImovel();
+                break;
+            case R.id.delete:
+                deleteImovel(arrayList.get(select));
+                break;
+            case R.id.settings:
+                configuracoes();
+                break;
+            case R.id.logout:
+                sair();
+                break;
+        }
+        return true;
+    }
+
+    public void addImovel() {
+        Intent intent = new Intent(this, AddImoveis.class);
+        startActivityForResult(intent, Constants.REQUEST_ADD_IMOVEL);
+    }
+    public void deleteImovel(Imoveis imovel){
+
+        if(imovel.getEmail().equals(mAuth.getCurrentUser().getEmail())){
+            databaseReference.child(imovel.getId()).removeValue();
+            arrayList.remove(imovel);
+            arrayAdapter.notifyDataSetChanged();
+        }
+        else{
+            Toast.makeText(this, "deu merda", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    public void sair() {
+        mAuth.signOut();
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void configuracoes() {
+        Intent intent = new Intent(this, Configuracoes.class);
+        startActivity(intent);
+    }
+
+    public void perfilUsuario(FirebaseUser currentUser) {
+        Intent intent = new Intent(this, PerfilUser.class);
+        intent.putExtra("email", currentUser.getEmail());
+        Log.v("DATA", currentUser.getUid());
+        startActivity(intent);
+    }
+
+}
